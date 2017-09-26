f4351222e4f50b057b214c011ecb5a23750e4816
phu hoa <phuhoadn95@gmail.com>
2017-09-26 13:43:20 +0700

    create notification when user invited to join campaign

---
+++ app/Notifications/InviteUser.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class InviteUser extends Notification
+{
+    use Queueable;
+
+    private $campaign;
+    private $user;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($user, $campaign)
+    {
+        $this->user = $user;
+        $this->campaign = $campaign;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    public function toDatabase($notifiable)
+    {
+        return [
+            'to' => $notifiable->id,
+            'from' => $this->user,
+            'campaign' => $this->campaign,
+        ];
+    }
+}
